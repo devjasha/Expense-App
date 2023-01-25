ecb4742dba3158e2769530016a1de0bed66c803e
jasha.chec <jasha@chec.de>
2023-01-25 12:40:28 +0100

    feat(main): split main in widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
-import './transaction.dart';
+import './widgets/user_transaction.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,88 +22,31 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
 
-  final List<Transaction> transactions = [
-    Transaction(id: 't1', title: 'New Shows', amount: 120.99, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Weekly Groceries', amount: 50.35, date: DateTime.now())
-  ];
+  // String titleInput = '';
+  // String amountInput = '';
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter App'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-        Container(
-          width: double.infinity,
-          child: const Card(
-            color: Colors.blue,
-            child: Text('Chart'),
-            elevation: 5,
-          )
+        appBar: AppBar(
+          title: const Text('Flutter App'),
         ),
-        Column(
-          children: transactions.map((transaction) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const SizedBox(
+                width: double.infinity,
                 child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 1,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  style: const TextStyle(
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  '\$ ${transaction.amount}'
-                                ),
-                              ],
-                            )
-                        ),
-                        Expanded(
-                          flex: 2,
-                            child: Container(
-                              margin: EdgeInsets.only(left: 20),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                    ),
-                                    transaction.title,
-                                  ),
-                                  Text(
-                                    style: const TextStyle(
-                                      fontSize: 12,
-                                      color: Colors.black26,
-                                    ),
-                                    DateFormat.yMMMd().format(transaction.date),
-                                  ),
-                                ],
-                              ),
-                            )
-                          )
-                      ],
-                    ),
-                  )
-                ),
-              );
-            }).toList(),
-        ),
-      ],)
-    );
+                  color: Colors.blue,
+                  elevation: 5,
+                  child: Text('Chart'),
+                )),
+            UserTransactions(),
+          ],
+        ));
   }
 }
