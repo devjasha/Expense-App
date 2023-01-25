57624304e2d84afccd3ed2a10cc5bd4ab8d11672
jasha.chec <jasha@chec.de>
2023-01-25 12:54:52 +0100

    feat(transaction_list): add scroll ability

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,60 +11,65 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((transaction) {
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                        flex: 1,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                                style: const TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                                '\$ ${transaction.amount}'),
-                          ],
-                        )),
-                    Expanded(
-                        flex: 2,
-                        child: Container(
-                          margin: const EdgeInsets.only(left: 20),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                ),
-                                transaction.title,
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: transactions.map((transaction) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                            flex: 1,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                    style: const TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                    '\$ ${transaction.amount}'),
+                              ],
+                            )),
+                        Expanded(
+                            flex: 2,
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                    transaction.title,
+                                  ),
+                                  Text(
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.black26,
+                                    ),
+                                    DateFormat.yMMMd().format(transaction.date),
+                                  ),
+                                ],
                               ),
-                              Text(
-                                style: const TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.black26,
-                                ),
-                                DateFormat.yMMMd().format(transaction.date),
-                              ),
-                            ],
-                          ),
-                        ))
-                  ],
-                ),
-              )),
-        );
-      }).toList(),
+                            ))
+                      ],
+                    ),
+                  )),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
